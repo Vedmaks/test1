0eabb8fe7bd0d167a6f84bcbc505e96aa8ad53eb
Vedmaks <70629659+Vedmaks@users.noreply.github.com>
2020-09-22 18:08:00 +0400

    Add files via upload

--- Marchenkov/script.js
+++ Marchenkov/script.js
@@ -12,6 +12,13 @@ products.push(
     createProduct('product4', 1, 400),
 )
 
+cartProducts.push(
+    createProduct('product1', 1, 100),
+    createProduct('product2', 1, 200),
+    createProduct('product3', 1, 300),
+    createProduct('product4', 1, 400),
+)
+
 
 function onLoad() {
 
@@ -29,21 +36,73 @@ function onLoad() {
 }
 
 function putInCart(num) {
+
     var cartTable = document.getElementById('cart');
-    var product = document.getElementById('product'+ num);
-    product.id = "cartProduct" + num;
-    product.onclick = "putInStorage(" + num + ")";
-    cartTable.appendChild(product);
+    var cartProduct = document.getElementById('cartProduct'+num);
+
+    if (cartProduct == null) {
+        var tr = document.createElement("tr");
+
+        tr.id = "cartProduct" + num;
+        tr.innerHTML += 
+        '<td>'+cartProducts[num]['name']+'</td>' + 
+        '<td>'+cartProducts[num]['count']+'</td>' + 
+        '<td>'+cartProducts[num]['price']+'</td>';
+        cartTable.appendChild(tr);
+    } else {
+        
+        var cartProd = cartProduct.getElementsByTagName("td")
+        var prodCount = document.createElement("td")
+        if (cartProducts[num]['count'] < products[num]['count']) {
+            cartProducts[num]['count']++;
+        }
+        prodCount.innerHTML = cartProducts[num]['count'];
+        cartProduct.replaceChild(prodCount, cartProd[1])
+        
+    }
+
+    var product = document.getElementById('product'+num)
+    var prod = product.getElementsByTagName("td")
+    var count = document.createElement("td")
+    count.innerHTML = products[num]['count'] - cartProducts[num]['count'];
+    product.replaceChild(count, prod[1])
+        
 }
 
 function putInStorage(num) {
-    var storageTable = document.getElementById('storage');
-    var cartProduct = document.getElementById('cartProduct'+ num);
-    cartProduct.onclick = "putInCart(" + num + ")";
-    cartProduct.id = "product" + num;
-    storageTable.appendChild(product);
+    var storageTable = document.getElementById('storeage');
+    var product = document.getElementById('product'+num);
+
+    if (product == null) {
+        var tr = document.createElement("tr");
+
+        tr.id = "product" + num;
+        tr.innerHTML += 
+        '<td>'+products[num]['name']+'</td>' + 
+        '<td>'+products[num]['count']+'</td>' + 
+        '<td>'+products[num]['price']+'</td>';
+        storageTable.appendChild(tr);
+    } else {
+        
+        var prod = product.getElementsByTagName("td")
+        var count = document.createElement("td")
+        if (products[num]['count'] < cartProducts[num]['count']) {
+            products[num]['count']++;
+        }
+        count.innerHTML = products[num]['count'];
+        product.replaceChild(count, prod[1])
+        
+    }
+
+    var cartProduct = document.getElementById('cartProduct'+num)
+    var cartProd = cartProduct.getElementsByTagName("td")
+    var cartCount = document.createElement("td")
+    cartCount.innerHTML = cartProducts[num]['count'] - products[num]['count'];
+    cartProduct.replaceChild(cartCount, cartProd[1])
+
 }
 
+function test() {
+    alert('good');
 
-
-
+};
